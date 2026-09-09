60b1b422712d41755493db1f06eb7bbc0f4c117c
Ana Eliza <anaelizacptx@gmail.com>
2026-09-09 17:57:16 -0300

    Add program to calculate rental car cost based on distance and days

---
+++ Ex014.py
@@ -0,0 +1,9 @@
+# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado 
+# e a quantidade de dias pelos quais ele foi alugado. 
+# Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+
+km = float(input('Digite a quantidade de Km percorridos: '))
+dias = int(input('Digite a quantidade de dias o carro foi alugado: '))
+total = (km * 0.15) + (dias * 60)
+
+print(f'O valor total a ser pago eh de R${total}')
