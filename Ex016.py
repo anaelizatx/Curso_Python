50dafad543a08055ef61652fd32ba96074101de3
Ana Eliza <anaelizacptx@gmail.com>
2026-09-09 17:57:48 -0300

    Add program to display the integer part of a real number

---
+++ Ex016.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia um numero real qualquer pelo teclado e mostre na tela sua posicao inteira.
+from math import floor
+
+num = float(input('Digite um numero:'))
+
+print(f'A posicao inteira deste numero eh: {floor(num)}')
